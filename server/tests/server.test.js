0b62e41cbaadf404f605283a88ba90f606c82ce1
rendlik <rsonroad@gmail.com>
2018-08-21 12:05:57 -0700

    add tests for delete route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,41 @@ describe('GET /todos/:id',() =>{
             .end(done)
     })
 })
+
+describe('DELETE /todos/:id',()=>{
+    it('should delete a todo',(done)=>{
+        const hexID = todos[1]._id.toHexString()
+
+        request(app)
+            .delete(`/todos/${hexID}`)
+            .expect(200)
+            .expect(res=>{
+                expect(res.body.todo._id).toBe(hexID)
+            })
+            .end((err,res)=>{
+                if(err){
+                    return done(err)
+                }
+
+                Todo.findById(hexID).then((todo) => {
+                    expect(todo).toBeNull();
+                    done()
+                }).catch(e=> done(e))
+            })
+    })
+
+    it('should return 404 if todo not found',(done)=>{
+        const hexID = new ObjectID().toHexString()
+                request(app)
+                    .delete(`/todos/${hexID}`)
+                    .expect(404)
+                    .end(done)
+    })
+
+    it('should return 404 if object id is invalid',(done)=>{
+        request(app)
+        .delete(`/todos/123abc`)
+        .expect(404)
+        .end(done)
+    })
+})
